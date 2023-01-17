88277634b1c98ecad9c592dab986c56ef1fcd6e3
M1rael3 <goodworkinspclass@mail.ru>
2023-01-17 10:48:43 +0700

    working with menu

--- app/src/main/java/com/example/clonegram/MainActivity.kt
+++ app/src/main/java/com/example/clonegram/MainActivity.kt
@@ -2,11 +2,100 @@ package com.example.clonegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.clonegram.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import com.mikepenz.materialdrawer.util.DrawerItemViewHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createHeader() {
+        header = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem()
+                    .withName("Buhstoevcky")
+                    .withEmail("goodworkin@mail.ru")
+            )
+            .build()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(binding.toolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .addDrawerItems(
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(100)
+                    .withIcon(R.drawable.ic_menu_create_groups)
+                    .withName("Создать группу"),
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(101)
+                    .withName("Контакты"),
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(102)
+                    .withName("Звонки"),
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(103)
+                    .withName("Люди рядом"),
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(104)
+                    .withName("Избранное"),
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(105)
+                    .withName("Настройки"),
+                DividerDrawerItem(),
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(106)
+                    .withName("Пригласить друзей"),
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(107)
+                    .withName("Возможности Clonegram")
+            )
+            .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    return false
+                }
+
+            })
+            .withAccountHeader(header)
+            .build()
     }
 }
